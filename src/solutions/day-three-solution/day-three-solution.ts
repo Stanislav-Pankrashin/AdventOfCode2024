c5aec10a2e0c3e505b5b632d9bd1cf66367aed89
Stanislav Pankrashin <stasera2@gmail.com>
2024-12-04 00:14:16 +1300

    Day three part two

--- src/solutions/day-three-solution/day-three-solution.ts
+++ src/solutions/day-three-solution/day-three-solution.ts
@@ -8,10 +8,13 @@ const decodeFunction = (mul: string): number => {
     return x * y;
 };
 
-export const dayThreeSolutionPartOne = (file: string) => {
+/**
+ * @param corruptedMemory a string that represents the memory
+ */
+const calculateMulInMemoryString = (corruptedMemory: string): number => {
     const regex = /mul[(](\d|\d\d|\d\d\d)[,](\d|\d\d|\d\d\d)[)]/g;
 
-    const matches = file.match(regex);
+    const matches = corruptedMemory.match(regex);
 
     if (matches === null) {
         return 0;
@@ -24,6 +27,23 @@ export const dayThreeSolutionPartOne = (file: string) => {
     return result;
 };
 
+export const dayThreeSolutionPartOne = (file: string) => {
+    return calculateMulInMemoryString(file);
+};
+
 export const dayThreeSolutionPartTwo = (file: string) => {
-    return 0;
+    // Split the memory into components
+    // First split into segments that start with do()
+    // Then split those segments by don't() and only take into account the first part as everything else is disabled
+    const memoryComponents = file
+        .split("do()")
+        .map((memoryFragment) => memoryFragment.split("don't()")[0]);
+
+    const result = memoryComponents.reduce(
+        (acc, memoryComponent) =>
+            acc + calculateMulInMemoryString(memoryComponent),
+        0,
+    );
+
+    return result;
 };
